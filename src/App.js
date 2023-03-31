248ed150cd1238d9501b5c6cedebbf6f1b0e71c9
akanoob <68201762+Animesh384@users.noreply.github.com>
2023-03-31 16:45:20 +0530

    mutple image upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+function ImageUploader() {
+  const [selectedFiles, setSelectedFiles] = useState([]);
+
+  const handleFileSelect = (event) => {
+    setSelectedFiles([...selectedFiles, ...event.target.files]);
+  }
+
+  const handleFileDelete = (file) => {
+    setSelectedFiles(selectedFiles.filter((f) => f !== file));
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const formData = new FormData();
+    for (let i = 0; i < selectedFiles.length; i++) {
+      formData.append('images', selectedFiles[i]);
+    }
+    fetch('https://reqres.in/api/post', {
+      method: 'POST',
+      body: formData
+    })
+    .then(response => {
+      if (response.ok) {
+        console.log('Form submitted successfully!');
+      } else {
+        console.error('Form submission failed.');
+      }
+    })
+    .catch(error => {
+      console.error('Error submitting form:', error);
+    });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <div style={{ display: 'flex', flexDirection: 'row' }}>
+        <input type="file" multiple onChange={handleFileSelect} />
+        <div style={{ marginLeft: '20px', display: 'flex', flexDirection: 'column' }}>
+          {selectedFiles.map((file, index) => (
+            <div key={file.name}>
+              <img src={URL.createObjectURL(file)} alt={file.name} style={{ width: '100px', height: '100px', objectFit: 'cover' }} />
+              <button type="button" onClick={() => handleFileDelete(file)}>Delete</button>
+            </div>
+          ))}
+        </div>
+      </div>
+      <button type="submit">Upload</button>
+    </form>
   );
 }
 
-export default App;
+export default ImageUploader;
